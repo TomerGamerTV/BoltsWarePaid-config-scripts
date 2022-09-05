5303a9a50485b2772f188f87decbf24f82091c24
Tomer <44266242+TomerGamerTV@users.noreply.github.com>
2022-09-05 14:21:11 -0700

    Add files via upload

---
+++ Qolts_SAVE_.lua
@@ -0,0 +1,754 @@
+return{
+    {
+        SetVal = '[Unknown Lua function: 0x00000000406fb2ff]',
+        Val = {
+            false
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000007d54489f]',
+        Val = {
+            false
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000054d1687f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000090f7618f]',
+        Val = {
+            1000
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000034fa8fbf]',
+        Val = {
+            100
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000035a8b217]',
+        Val = {
+            "HeadHB"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000da6a054f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000e80074f7]',
+        Val = {
+            false,
+            500
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000a17e509f]',
+        Val = {
+            "Baseball"
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000009856938f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000b60caecf]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003e8d3a3f]',
+        Val = {
+            false
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003675505f]',
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000000f9fdd7f]',
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000008bea856f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000008223684f]',
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000853dd62f]',
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000082b73937]',
+        Val = {
+            "Current"
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000002440bdb7]',
+        Val = {
+            false,
+            0
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000fa43cc4f]',
+        Val = {
+            "Spinning"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000005f86fc77]',
+        Val = {
+            true,
+            0
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003391634f]',
+        Val = {
+            "Down"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000653ebc37]',
+        Val = {
+            false,
+            0
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000704657b7]',
+        Val = {
+            "Offset"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000974069ef]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000290375f7]',
+        Val = {
+            false,
+            2
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000f8c90faf]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000007642253f]',
+        Val = {}
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000cc47700f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000d675b39f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000faed7b5f]',
+        Val = {
+            100
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000009a922d0f]',
+        Val = {
+            100
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000dfc434b7]',
+        Val = {
+            false,
+            0
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000788ed00f]',
+        Val = {
+            "Spinning"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000f28feb77]',
+        Val = {
+            false,
+            0
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000034c1f8af]',
+        Val = {
+            "Absolute"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000ce68ee6f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000007520349f]',
+        Val = {
+            2500
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000009ad3c4cf]',
+        Val = {
+            100
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000ca4a4d37]',
+        Val = {
+            false,
+            5
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000544676df]',
+        Val = {
+            Color3.new(1, 0, 0.686275),
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000001e510ccf]',
+        Val = {
+            "SmoothPlastic"
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000005ffb6ef7]',
+        Val = {
+            true,
+            100
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000010956bbf]',
+        Val = {
+            "V"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000042588bb7]',
+        Val = {
+            false,
+            16
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000921dd88f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000042b89677]',
+        Val = {
+            false,
+            1
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000336d6747]',
+        Val = {
+            "Legit"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000009b6466ef]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000001efbcfff]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000008159faaf]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000000ccc043f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000000847e65f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000f7ca9f6f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000f5fec02f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000696392bf]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000e39117ef]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000067367eff]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000005e0f8c9f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000d9846baf]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003a2afa1f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000bcb5132f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000004c35c67]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000834ee757]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003ac40cbf]',
+        Val = {
+            60
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000dab5db2f]',
+        Val = {
+            60
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000009c555537]',
+        Val = {
+            false
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000088bb1397]',
+        Val = {
+            false
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000008da2e9f7]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000301d96e7]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000b6e83fd7]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003b751087]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000bffe6db7]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003a0bca27]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000a4849b17]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000079a0a15f]',
+        Val = {
+            Color3.new(0, 1, 0.733333),
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000006c97fa7f]',
+        Val = {
+            Color3.new(0, 255, 0),
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000013fcb41f]',
+        Val = {
+            Color3.new(255, 0, 0),
+            false
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000dfcb5637]',
+        Val = {
+            true,
+            9
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000001d8b19df]',
+        Val = {
+            Color3.new(0.768627, 0, 1),
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000f50a5bf7]',
+        Val = {
+            true,
+            9
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000000fa67d9f]',
+        Val = {
+            Color3.new(0, 1, 0.733333),
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000002cad2c87]',
+        Val = {
+            "SmoothPlastic"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000b53c41df]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000366c283f]',
+        Val = {
+            Color3.new(0.768627, 0, 1),
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000353aa57f]',
+        Val = {
+            12
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000005a48ca6f]',
+        Val = {
+            2
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000b825abff]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000b0b71eb7]',
+        Val = {
+            false,
+            5
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003459537f]',
+        Val = {
+            Color3.new(0.768627, 0, 1),
+            false
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000005c04fb7f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000d1bf524f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000004a33ad1f]',
+        Val = {
+            "PC"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000d496f42f]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000003fb26377]',
+        Val = {
+            "None"
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x0000000044afae0f]',
+        Val = {
+            50
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000404bfe3f]',
+        Val = {
+            120
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000c5062baf]',
+        Val = {
+            45
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000007a5a919f]',
+        Val = {
+            10
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        Val = {
+            true
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000c6fa43ef]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x000000005b98ba37]',
+        Val = {
+            false
+        }
+    },
+    {
+        SetVal = '[Unknown Lua function: 0x00000000dfe553a7]',
+        Val = {
+            false
+        }
+    }
+}
